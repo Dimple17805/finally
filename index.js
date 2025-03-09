3a148e2fc44e997e2133e95df998f9153f2818bc
Dimple17805 <dimplesehal5@gmail.com>
2025-03-09 23:48:22 +0530

    Updated frontend UI

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["final-iota-beige.vercel.app" , "http://localhost:3000"] ,
+    origin: ["https://final-iota-beige.vercel.app/" , "http://localhost:3000"] ,
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
